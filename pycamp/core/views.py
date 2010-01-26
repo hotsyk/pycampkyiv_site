ac5eb4896e3f30d3137e46cd171e02c38522d6fc
Volodymyr Hotsyk <vhotsyk@desktop.hotsyk.com>
2010-01-26 12:16:44 +0200

    Localization for schedule, closed registration

--- pycamp/core/views.py
+++ pycamp/core/views.py
@@ -29,6 +29,63 @@ def main_page(request):
         pycamp_registration = u"Registration"
         pycamp_contacts = u"Contacts"
         pycamp_tag = u"Hash tag"
+        pycamp_registration2=u"Registration is closed"
+        schedule = u"""
+<tr>
+   <td > 09:00</td><td>09:45</td><td colspan="2">Registration</td>
+</tr>
+<tr>
+    <td>09:45</td><td>10:10</td><td colspan="2">Introduction</td>
+</tr>
+<tr>
+    <td>10:10</td><td>10:50</td><td>Why Python is the brake and how to make it little more faster.</td><td>   Alexander Shigin</td>
+</tr>
+<tr>
+    <td>10:50</td><td>11:30</td><td>Decorators' recipes </td><td>Yury Yurevich</td>
+</tr>
+<tr>
+    <td>11:30</td><td>11:50</td><td colspan="2">Coffee break</td>
+</tr>
+<tr>
+    <td>11:50</td><td>12:10</td><td>Programming on the nerves</td><td>Dmitry Kozhevin</td>
+</tr>
+<tr>
+    <td>12:10</td><td>12:50</td><td>Working with the Google App Engine datastore, differences from the relational model.</td><td> Mikhail Kashkin</td>
+</tr>
+<tr>
+    <td>12:50</td><td>13:20</td><td> Redis: Wild West of databases </td><td>Alexander Solovyov</td>
+</tr>
+<tr>
+    <td>13:20</td><td>14:45</td><td colspan="2">Lunch</td>
+</tr>
+<tr>
+    <td>14:45</td><td>15:30</td><td>Safe software development as a result of the long way and many bumps filled. </td><td>Andrew Svetlov</td>
+</tr>
+<tr>
+    <td>15:30</td><td>16:10</td><td>Python: Extending and Embedding</td><td>Vladimir Pouzanov, Vladimir Kirillov</td>
+</tr>
+<tr>
+    <td>16:10</td><td>16:20</td><td colspan="2">Coffee break</td>
+</tr>
+<tr>
+    <td>16:20</td><td>16:50</td><td> Using Python in GIS </td><td>Andrii Mishkovskyi</td>
+</tr>
+<tr>
+    <td>16:50</td><td>17:20</td><td>WebSockets in Twisted </td><td>Sergey Kirillov</td>
+</tr>
+<tr>
+    <td>17:20</td><td>17:30</td><td colspan="2"> Coffee break</td>
+</tr>
+<tr>
+    <td>17:30</td><td>18:00</td><td>Internationalization and localization of the Python-applications with the gettext.</td><td>Alexander Belchenko</td>
+</tr>
+<tr>
+    <td>18:00</td><td>18:30</td><td> Working with the payment systems in Django (PayPal, WebMoney).</td><td> Ivan Morgun</td>
+</tr>
+<tr>
+    <td>18:30</td><td>18:50</td><td>PyCharm – new python IDE from JetBrains</td><td> Dmitry Jemerov</td>
+</tr>
+         """
     elif int(lang) == 2:
         speaker_title = u'Докладчики'
         speak_title = u"Тема доклада"
@@ -40,6 +97,63 @@ def main_page(request):
         pycamp_registration = u"Регистрация"
         pycamp_contacts = u"Контакты"
         pycamp_tag = u"Хеш тег"
+        pycamp_registration2=u"Регистрация закрыта"
+        schedule = u"""
+<tr>
+   <td > 09:00</td><td>09:45</td><td colspan="2">Регистрация</td>
+</tr>
+<tr>
+    <td>09:45</td><td>10:10</td><td colspan="2">Вступительное слово</td>
+</tr>
+<tr>
+    <td>10:10</td><td>10:50</td><td>Почему Python - тормоз и как заставить его меньше тормозить.</td><td>   Александр Шигин</td>
+</tr>
+<tr>
+    <td>10:50</td><td>11:30</td><td>Рецепты декораторов</td><td>Юрий Юревич</td>
+</tr>
+<tr>
+    <td>11:30</td><td>11:50</td><td colspan="2">Кофе-брейк</td>
+</tr>
+<tr>
+    <td>11:50</td><td>12:10</td><td>Программирование на нервах</td><td>Дмитрий Кожевин</td>
+</tr>
+<tr>
+    <td>12:10</td><td>12:50</td><td>Работа с хранилищем данных в Google App Engine, отличия от реляционной модели.</td><td> Михаил Кашкин</td>
+</tr>
+<tr>
+    <td>12:50</td><td>13:20</td><td>    Redis: Дикий Запад баз данных   </td><td>Александр Соловьев</td>
+</tr>
+<tr>
+    <td>13:20</td><td>14:45</td><td colspan="2">Обед</td>
+</tr>
+<tr>
+    <td>14:45</td><td>15:30</td><td> Безопасная разработка ПО. Результат длинного пути и множества набитых шишек. </td><td>   Андрей Светлов</td>
+</tr>
+<tr>
+    <td>15:30</td><td>16:10</td><td>Расширение и встраивание Python</td><td> Владимир Пузанов, Владимир Кириллов</td>
+</tr>
+<tr>
+    <td>16:10</td><td>16:20</td><td colspan="2">Кофе-брейк  </td>
+</tr>
+<tr>
+    <td>16:20</td><td>16:50</td><td> Использование Python в ГИС </td><td> Андрей Мишковский</td>
+</tr>
+<tr>
+    <td>16:50</td><td>17:20</td><td>WebSockets в Twisted  </td><td>  Сергей Кириллов</td>
+</tr>
+<tr>
+    <td>17:20</td><td>17:30</td><td colspan="2"> Кофе-брейк  </td>
+</tr>
+<tr>
+    <td>17:30</td><td>18:00</td><td>Интернационализация и локализация Python-приложений с использованием gettext.  </td><td> Александр Бельченко</td>
+</tr>
+<tr>
+    <td>18:00</td><td>18:30</td><td> Работа с платежными системами в Django (PayPal, WebMoney). </td><td> Иван Моргун</td>
+</tr>
+<tr>
+    <td>18:30</td><td>18:50</td><td>PyCharm – новая python IDE от JetBrains </td><td> Дмитрий Жемеров</td>
+</tr>
+         """
     else:
         speaker_title = u'Доповідачі'
         speak_title = u"Тема доповіді"
@@ -51,7 +165,63 @@ def main_page(request):
         pycamp_registration = u"Реєстрація"
         pycamp_contacts = u"Контакти"
         pycamp_tag = u"Хеш тег"
-
+        pycamp_registration2=u"Реєстрацію припинено"
+        schedule = u"""
+<tr>
+   <td > 09:00</td><td>09:45</td><td colspan="2">Реєстрація</td>
+</tr>
+<tr>
+    <td>09:45</td><td>10:10</td><td colspan="2">Вітальне слово</td>
+</tr>
+<tr>
+    <td>10:10</td><td>10:50</td><td>Чому Python - гальмо і як змусити його гальмувати менше</td><td> Олександр Шигін</td>
+</tr>
+<tr>
+    <td>10:50</td><td>11:30</td><td>Рецепти декораторів</td><td>Юрій Юревіч</td>
+</tr>
+<tr>
+    <td>11:30</td><td>11:50</td><td colspan="2">Кофе-брейк</td>
+</tr>
+<tr>
+    <td>11:50</td><td>12:10</td><td>Програмування на нервах</td><td>Дмитро Кожевін</td>
+</tr>
+<tr>
+    <td>12:10</td><td>12:50</td><td>Робота зі сховищем даних в Google App Engine, відмінності від реляційної моделі</td><td>Михайло Кашкін</td>
+</tr>
+<tr>
+    <td>12:50</td><td>13:20</td><td>   Redis: Дикий Захід баз даних  </td><td>Олександр Соловйов</td>
+</tr>
+<tr>
+    <td>13:20</td><td>14:45</td><td colspan="2">Обід</td>
+</tr>
+<tr>
+    <td>14:45</td><td>15:30</td><td>Безпечна розробка ПЗ. Результат довгого шляху та величезної кількості набитих гуль.</td><td> Андрій Светлов</td>
+</tr>
+<tr>
+    <td>15:30</td><td>16:10</td><td>Розширення та вбудовування Python</td><td>Володимир Пузанов, Володимир Кирилов</td>
+</tr>
+<tr>
+    <td>16:10</td><td>16:20</td><td colspan="2">Кофе-брейк  </td>
+</tr>
+<tr>
+    <td>16:20</td><td>16:50</td><td> Використання Python в ГІС </td><td> Андрій Мішковський</td>
+</tr>
+<tr>
+    <td>16:50</td><td>17:20</td><td>WebSockets в Twisted</td><td> Сергій Кіріллов</td>
+</tr>
+<tr>
+    <td>17:20</td><td>17:30</td><td colspan="2"> Кофе-брейк  </td>
+</tr>
+<tr>
+    <td>17:30</td><td>18:00</td><td>Інтернаціоналізація та локалізація Python-застосувань з використанням gettext.</td><td> Олександр Бєльченко</td>
+</tr>
+<tr>
+    <td>18:00</td><td>18:30</td><td>Робота з платіжними системами в Django (PayPal, WebMoney).</td><td> Іван Моргун</td>
+</tr>
+<tr>
+    <td>18:30</td><td>18:50</td><td>PyCharm – нова python IDE від JetBrains </td><td> Дмитрj Жемеров</td>
+</tr>
+         """
     langs = ""
     for l in LANGUAGE_CHOICES:
         if not int(lang) == l[0]:
@@ -103,7 +273,8 @@ def main_page(request):
         'twitter_title' : twitter_title,
         'special_title' : special_title,
         'flash_news': flash_news,
-
+        'pycamp_registration2': pycamp_registration2,
+        'schedule': schedule,
     })
     return render_to_response('core/appbase.html', context)
 
